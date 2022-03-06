d852c1de51bc2a06b1af41921c3443092730cfe1
Artyom Nelyub <artyom.nelyub.an@gmail.com>
2022-03-06 09:55:32 +0400

    Добавил зависимости leaflet

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -4,7 +4,6 @@ import SVGContainer from '../svg-container/svg-container';
 import Header from '../header/header';
 import Map from '../map/map';
 
-
 type MainScreenProps = {
   countOffers: number;
 };
@@ -23,9 +22,7 @@ function MainScreen(mainScreenProps: MainScreenProps): JSX.Element {
               <ul className="locations__list tabs__list">
                 <li className="locations__item">
                   <a className="locations__item-link tabs__item" href="#todo">
-                    <span>
-                      Paris
-                    </span>
+                    <span>Paris</span>
                   </a>
                 </li>
                 <li className="locations__item">
@@ -93,11 +90,11 @@ function MainScreen(mainScreenProps: MainScreenProps): JSX.Element {
                   </ul>
                 </form>
                 <div className="cities__places-list places__list tabs__content">
-                  <OffersList offers={offers}/>
+                  <OffersList offers={offers} />
                 </div>
               </section>
               <div className="cities__right-section">
-                <Map cityLocation={cityLocation} rentPoints={rentPoints}/>
+                <Map cityLocation={cityLocation} rentPoints={rentPoints} />
               </div>
             </div>
           </div>
--- project/src/mocks/mocks.ts
+++ project/src/mocks/mocks.ts
@@ -7,7 +7,7 @@ export const cityLocation: CityLocation = {
 };
 
 export const rentPoints: RentPoints = [
-  { latitude: 52.3909553943508,  longitude: 4.85309666406198 },
+  { latitude: 52.3909553943508, longitude: 4.85309666406198 },
   { latitude: 52.369553943508, longitude: 4.85309666406198 },
   { latitude: 52.3909553943508, longitude: 4.929309666406198 },
   { latitude: 52.3809553943508, longitude: 4.939309666406198 },
